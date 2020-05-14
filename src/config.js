6e776d0abbcb90e6c9d01468793cb55430f5d793
Alex Fryer <alex.jeffrey.fryer@gmail.com>
2020-05-13 21:47:15 -0700

    fixed deploy url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
+  process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
 export const ROACH_OPTIONS = ['equalWeight', 'minimumVariance'];
 
